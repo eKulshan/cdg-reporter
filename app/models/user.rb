8c6a5ce712f1134b5b095c0cd400babf93da39ce
Evgeniy Kulshan <eikulshan@gmail.com>
2022-03-07 09:58:54 +0300

    add roles

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,11 +2,32 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  :recoverable, :rememberable, :validatable
 
   validates :email, length: { in: 3..150 }
   validates :first_name, presence: true, length: { in: 3..100 }
   validates :last_name, presence: true, length: { in: 3..100 }
 
+  belongs_to :role
   has_many :reports, dependent: :destroy
+
+  before_validation :set_default_role
+
+  def admin?
+    self.role.name == 'admin'
+  end
+
+  def reviewer?
+    self.role.name == 'reviwer'
+  end
+
+  def author?(report)
+    self[:id] == report[:user_id]
+  end
+
+  private
+  def set_default_role
+    self.role ||= Role.find_by_name('student')
+    pp self
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_16_120521) do
+ActiveRecord::Schema.define(version: 2022_03_16_120523) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,12 @@ ActiveRecord::Schema.define(version: 2022_03_16_120521) do
     t.string "description"
     t.string "grade"
     t.bigint "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "roles", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
@@ -34,8 +40,11 @@ ActiveRecord::Schema.define(version: 2022_03_16_120521) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "role_id", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["role_id"], name: "index_users_on_role_id"
   end
 
+  add_foreign_key "users", "roles"
 end
